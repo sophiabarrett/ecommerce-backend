8ce07d02dc5e5d15d99676ca63903ea643824023
Sophia Barrett <sophia@yoursummit.media>
2021-12-04 16:01:03 -0700

    refactor

--- models/index.js
+++ models/index.js
@@ -1,16 +1,16 @@
 // import models
-const Product = require('./Product');
 const Category = require('./Category');
+const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
+// Categories have many Products
+Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 
-// Categories have many Products
-Category.hasMany(Product, {
+// Products belongsTo Category
+Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 
